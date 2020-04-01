66e0903b0c9a92562478ed092d009b89ccf80123
billie.pratama <Billie.Pratama@commbank.co.id>
2020-04-01 13:15:52 +0700

    Learn more -> Billie Pratama

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Billie Pratama
         </a>
       </header>
     </div>
