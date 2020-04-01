d2e78b42bcc3c2a05e9fe10f38f64480bd8d0434
billie.pratama <Billie.Pratama@commbank.co.id>
2020-04-01 13:24:41 +0700

    Billie Pratama -> Testing aja

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Billie Pratama
+          Testing aja sih 
         </a>
       </header>
     </div>
